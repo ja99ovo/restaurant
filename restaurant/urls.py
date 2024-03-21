acbda06f2096e0a7a01ee44c337b790439ed250c
Yulei-cn <116038178+Yulei-cn@users.noreply.github.com>
2024-03-21 13:21:03 +0100

    2.0
    整理布局添加新桌子

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.table_list, name='table_list'),
 
     path('add_order_item/', views.add_order_item, name='add_order_item'),
-
+    path('add_table/', views.add_table, name='add_table'),
     path('table/<int:table_id>/add_order_item/<int:order_id>/', views.add_order_item, name='add_order_item_with_order'),
     path('order/<int:order_id>/', views.order_detail, name='order_detail'),
     path('cashier/', views.cashier_summary, name='cashier_summary'),
