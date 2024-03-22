2a9475546e18cf274259b0b4cfe047cf0fc7d33b
HUANG Zhaoyuan <104513555+ja99ovo@users.noreply.github.com>
2024-03-22 16:57:50 +0100

    订单修改
    创建新订单后重定向至订单修改页面
    增加了修改订单功能

--- restaurant/forms.py
+++ restaurant/forms.py
@@ -10,3 +10,14 @@ class New_order_form(forms.Form):
         super().__init__(*args, **kwargs)
         for boisson in Boisson.objects.all():
             self.fields[f'boisson_{boisson.name}'] = forms.CharField(label=f'{boisson.name}' , initial=0,widget=forms.HiddenInput())
+            
+            
+            
+class Change_order_form(forms.Form):
+    adults= forms.IntegerField(label='adults' , initial=0,widget=forms.HiddenInput())
+    kids = forms.IntegerField(label='kids' , initial=0,widget=forms.HiddenInput())
+    toddlers = forms.IntegerField(label='toddlers' , initial=0,widget=forms.HiddenInput()) 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for boisson in Boisson.objects.all():
+            self.fields[f'boisson_{boisson.name}'] = forms.CharField(label=f'{boisson.name}' , initial=0,widget=forms.HiddenInput())
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,6 @@
 from .models import tables, orders, OrderItem,Boisson,table_order
 from django.shortcuts import render, redirect, get_object_or_404
-from .forms import New_order_form
+from .forms import New_order_form, Change_order_form
 from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponseRedirect
@@ -9,9 +9,9 @@ from django.urls import reverse
 def table_list(request):
     table_list = tables.objects.all()
     for t in table_list:
-        active_order=table_order.objects.filter(related_table_id=t.id,status='Active').first()
+        order_active=table_order.objects.filter(related_table_id=t.id,status='Active').first()
         t.active=table_order.objects.filter(related_table_id=t.id,status='Active').exists()
-        t.active_id=active_order.related_order_id if t.active else None
+        t.active_id=order_active.related_order_id if t.active else None
     return render(request, 'restaurant/table_list.html', {'tables': table_list})
 
 def add_table(request):
@@ -25,20 +25,41 @@ def add_table(request):
 def table_detail(request, table_id):
     pass
 
-def order_detail(request, order_id):
-    # 假设 table_order 模型关联到了订单和桌子
-    order = get_object_or_404(table_order, id=order_id)
-
-    # 假设 OrderItem 包含了订单中的项目信息
-    items = OrderItem.objects.filter(order=order)
-
-    # 计算订单总额
-    total = sum(item.boisson.prix * item.quantity for item in items)
-
+def order_detail(request):
+    table_id = request.GET.get('table_id')
+    boissons=Boisson.objects.all()
+    table_obj=tables.objects.get(id=table_id)
+    
+    order_id=table_order.objects.filter(related_table_id=table_id,status='Active').values_list(
+            'related_order_id',flat=True).first()
+    order_obj=orders.objects.filter(id=order_id).first()
+    boisson_ordered=OrderItem.objects.filter(order_id=order_id).all()
+    if request.method == 'POST':
+        form = Change_order_form(request.POST)
+        if form.is_valid():
+            adults=form.cleaned_data.get('adults')
+            toddlers=form.cleaned_data.get('toddlers')
+            kids=form.cleaned_data.get('kids')
+            order_obj.adults=adults
+            order_obj.kids=kids
+            order_obj.toddlers=toddlers
+            order_obj.save()
+            
+        for b in boissons:
+            quantity=form.cleaned_data.get(f'boisson_{b.name}')
+            b_old=boisson_ordered.filter(boisson=b).all().first()
+            b_old.quantity=quantity
+            b_old.save()
+            
+    form = Change_order_form()
+    for b in boissons:
+        b.quantity = boisson_ordered.filter(boisson_id=b.id).values_list("quantity",flat=True).first() if boisson_ordered.filter(boisson_id=b.id).exists() else 0
     return render(request, 'restaurant/order_detail.html', {
-        'order': order,
-        'items': items,
-        'total': total
+        'adults':order_obj.adults,
+        'kids':order_obj.kids,
+        'toddlers':order_obj.toddlers,
+        'form':form,
+        'boissons': boissons
     })
 
 
@@ -66,15 +87,25 @@ def add_order_item(request):
                 quantity=form.cleaned_data.get(f'boisson_{b.name}')
                 new_order_item=OrderItem(quantity=quantity,boisson=b,order=new_order)
                 new_order_item.save()
-            return HttpResponseRedirect('')
+                
+            #重定向到订单修改页面
+            new_form=Change_order_form()
+            boisson_ordered=OrderItem.objects.filter(order=new_order).all()
+            for b in boissons:
+                b.quantity = boisson_ordered.filter(boisson_id=b.id).values_list("quantity",flat=True).first() if boisson_ordered.filter(boisson_id=b.id).exists() else 0
+            return render(request, 'restaurant/order_detail.html', {
+                'adults':adults,
+                'kids':kids,
+                'toddlers':toddlers,
+                'form':new_form,
+                'boissons': boissons
+            })
         else:
             print("not valid")
     else:
         form = New_order_form()
     return render(request, 'restaurant/add_order_item.html', {'form':form,'boissons': boissons})
 
-def order_detail(request, order_id):
-    pass
 
 def cashier_summary(request):
     pass
