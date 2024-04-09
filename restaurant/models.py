aa56fb9b4b40eb5e1c98dca36d1c9e55eeb80b64
Yulei-cn <116038178+Yulei-cn@users.noreply.github.com>
2024-04-09 10:45:56 +0200

    Update models.py

--- restaurant/models.py
+++ restaurant/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Boisson(models.Model):
 
     name=models.CharField(max_length=20)
-    prix=models.IntegerField()
+    prix=models.FloatField()
 
 class Table(models.Model):
     name=models.CharField(max_length=20,null=True)
