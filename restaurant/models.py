f196984d5450e3c090e81ec4ab4956edaaeb18b6
Yulei-cn <116038178+Yulei-cn@users.noreply.github.com>
2024-04-17 17:09:22 +0200

    增加长度

--- restaurant/models.py
+++ restaurant/models.py
@@ -6,7 +6,7 @@ class Category(models.Model):
         return self.name
 
 class Boisson(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=100)
     prix = models.DecimalField(max_digits=5, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='boissons')
 
